3f7eb1289650dee07c2b7b1fcd305dbdf290256a
Pentaho\shanush <shanush@pentaho.com>
2018-01-08 12:18:17 -0500

    added surefire plugin and CL execution capability

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Selenide-POM-TestNG
 
+TO Configure startup of the tests you can change setting in local.properties files and set
+desired options. Description can be found at http://selenide.org/javadoc in com.codeborne.selenide.Configuration package
 
-In order to run test open src/test/XMLSuites folder and select Run through right click.
+Main of them are:
+- selenide.browser - type of browser you want to start
+- selenide.baseUrl - url of application tested
+- remote - url of selenium hub
+
+In order to run test open src/test/XMLSuites folder in IDE and select Run through right click on the test you want to run.
+
+To run from comand line need to execute <<mvn -Dsurefire.suiteXmlFiles="fullPathToXmlSuite" test >> command
+
+
 NOTE: not test configured to run on locally installed Pentaho
+
+
--- src/main/java/com/hv/pages/DataSources/ManageDataSourcesPage.java
+++ src/main/java/com/hv/pages/DataSources/ManageDataSourcesPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,13 +14,15 @@ import static com.codeborne.selenide.Selenide.$;
 public class ManageDataSourcesPage  {
     private static final Logger LOGGER = Logger.getLogger(ManageDataSourcesPage.class);
 
+    @FindBy(xpath="//div[@class='Caption' and text()='Manage Data Sources']")
+    protected SelenideElement lblManageDSl;
 
     private SelenideElement expectedDS(String expectedDatasource) {
         return $(By.xpath("//div[text()='" + expectedDatasource + "']"));
     }
 
-
     public void verifyDScreated(String expectedDatasource) {
+        lblManageDSl.waitUntil(Condition.visible,5000);
         expectedDS(expectedDatasource).shouldBe(Condition.exist);
     }
 
